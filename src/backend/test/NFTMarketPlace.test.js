f6ea0b9e73816e4d3bb62146d06ee089ce856253
jjeg1979 <jjeg1979@gmail.com>
2024-12-26 08:34:20 +0100

    Testing suite

--- src/backend/test/NFTMarketPlace.test.js
+++ src/backend/test/NFTMarketPlace.test.js
@@ -1,7 +1,10 @@
+/* eslint-disable jest/valid-expect */
+/* eslint-disable no-undef */
 const { expect } = require("chai");
 
 const toWei = (num) => ethers.utils.parseEther(num.toString());
 const fromWei = (num) => ethers.utils.formatEther(num);
+
 
 describe("NFTMarketplace", function () {
 
@@ -40,6 +43,98 @@ describe("NFTMarketplace", function () {
         it("Should track feeAccount and feePercent", async function () {
             expect(await marketplace.feeAccount()).to.equal(deployer.address);
             expect(await marketplace.feePercent()).to.equal(feePercent);
+        });
+    });
+
+    describe("Minting NFTs", function () {
+        it("Should track each minted NFT", async function () {
+            await nft.connect(addr1).mint(URI);
+            expect(await nft.tokenCount()).to.equal(1);
+            expect(await nft.balanceOf(addr1.address)).to.equal(1);
+            expect(await nft.tokenURI(1)).to.equal(URI);
+
+            await nft.connect(addr2).mint(URI);
+            expect(await nft.tokenCount()).to.equal(2);
+            expect(await nft.balanceOf(addr2.address)).to.equal(1);
+            expect(await nft.tokenURI(2)).to.equal(URI);
+        });
+    });
+
+    describe("Making marketplace items", function () {
+        let price = 1;
+        let result;
+
+        beforeEach(async function () {
+            await nft.connect(addr1).mint(URI);
+            await nft.connect(addr1).setApprovalForAll(marketplace.address, true);
+        });
+
+        it("Should track newly created item", async function () {
+            await expect(marketplace.connect(addr1).makeItem(nft.address, 1, toWei(price)))
+                .to.emit(marketplace, "Offered")
+                .withArgs(
+                    1,
+                    nft.address,
+                    1,
+                    toWei(price),
+                    addr1.address
+                );
+
+
+            expect(await nft.ownerOf(1)).to.equal(marketplace.address);
+            expect(await marketplace.itemCount()).to.equal(1);
+
+            const item = await marketplace.items(1);
+            expect(item.itemId).to.equal(1);
+            expect(item.nft).to.equal(nft.address);
+            expect(item.tokenId).to.equal(1);
+            expect(item.price).to.equal(toWei(price));
+            expect(item.sold).to.equal(false);
+        });
+
+        it("Should fail if fail prize is set to zero", async function () {
+            await expect(marketplace.connect(addr1).makeItem(
+                nft.address, 1, 0)).to.be.revertedWith("Price must be greater than zero");
+        });
+    });
+
+    describe("Purchasing marketplace items", function () {
+        let price = 2;
+        let fee = (feePercent / 100) * price
+        let totalPriceInWei;
+
+        beforeEach(async function () {
+            await nft.connect(addr1).mint(URI);
+            await nft.connect(addr1).setApprovalForAll(marketplace.address, true);
+
+            await marketplace.connect(addr1).makeItem(
+                nft.address,
+                1,
+                toWei(price)
+            );
+        });
+
+        it("Should update item as sold, pay seller, transfer to buyer...", async function () {
+            const sellerInitialEthBal = await addr1.getBalance();
+            const feeAccountInitialEthBal = await deployer.getBalance();
+            totalPriceInWei = await marketplace.getTotalPrice(1);
+            await expect(marketplace.connect(addr2).purchaseItem(1, { value: totalPriceInWei })).to.emit(marketplace, "Bought").withArgs(
+                1,
+                nft.address,
+                1,
+                toWei(price),
+                addr1.address,
+                addr2.address
+            );
+
+            const sellerFinalEthBal = await addr1.getBalance();
+            const feeAccountFinalEthBal = await deployer.getBalance();
+
+            expect((await marketplace.items(1)).sold).to.equal(true);
+            expect(+fromWei(sellerFinalEthBal)).to.equal(+price + +fromWei(sellerInitialEthBal));
+            expect(+fromWei(feeAccountFinalEthBal)).to.equal(+fee + +fromWei(feeAccountInitialEthBal));
+            expect(await nft.ownerOf(1)).to.equal(addr2.address);
+
         });
     });
 })
